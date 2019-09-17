8aec1326afd24dfeaf82745a81277ae7070f590d
Jorge Toledano <kalimantole@gmail.com>
2019-09-17 12:06:26 -0500

    Actualizaciones de seguridad desde master

---
+++ tole_retrieve_dataset.py
@@ -0,0 +1,20 @@
+"""
+Script de prueba para descargar datasets con
+una etiqueta predeterminada.
+
+"""
+
+import json
+import requests
+
+tole_token = []
+
+def docker_c_copy(container_id, password):
+
+
+
+
+def import_dw_datasts(token, record_id):
+    tkn = open('~/src/toletoken.txt')
+    return tkn
+    
